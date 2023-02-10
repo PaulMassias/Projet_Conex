e9efe6759e5485a12755a790a9bc4aa4488b956f
Anaelle Jolivet <anaelle0jolivet@gmail.com>
2023-02-10 13:59:49 +0100

    readme + fonction sans paramètre v1

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ composer install
 
 symfony server:start -d
 symfony open:local
+
+Pour installer si il ne reconnais pas les use importés
+symfony composer require symfony/serializer-pack
--- src/Controller/MainAPIController.php
+++ src/Controller/MainAPIController.php
@@ -16,7 +16,52 @@ use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class MainAPIController extends AbstractController
 {
+    #[Route('/tc', name: 'app_tc_API')]
+    public function resultatTC(){
+        $consoDeplacementMed = 0.193*10*2;
 
+        $consoTelemedecine = 0.0008515;
+        $consoFichier = 0.1;
+        $consoVisio = 0.000125;
+        
+        $emission = $consoTelemedecine + $consoFichier + $consoVisio;
+        
+        $gain = $consoDeplacementMed - $emission;
+
+        $resultat = new TcResult();
+        $resultat
+            ->setTitle('Gain Teleconsultation')
+            ->setResultat($gain)
+        ;
+        $normalizers = [
+            new ObjectNormalizer(),
+        ];
+
+        $encoders =[
+            new JsonEncoder(),
+        ];
+
+        $serializer = new Serializer($normalizers,$encoders);
+        $SerData = $serializer->serialize($resultat, 'json');
+
+        $response = new Response($SerData);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+    public function resultatTCParam($tailleFichier, $tempsVisio){
+        $consoDeplacementMed = 0.193*10*2;
+
+        $consoTelemedecine = 0.0008515;
+        $consoFichier = 0.1 * $tailleFichier;
+        $consoVisio = 0.000125 * $tempsVisio;
+
+        $emission = $consoTelemedecine + $consoFichier + $consoVisio;
+        
+        $resultat = $consoDeplacementMed - $emission;
+        return $resultat;
+    }
 
 
     #[Route('/main', name: 'app_main_API')]
